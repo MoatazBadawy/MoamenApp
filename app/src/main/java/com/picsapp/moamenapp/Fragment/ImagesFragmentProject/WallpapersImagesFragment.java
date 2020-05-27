4c4e805d34f0e1067aeaa6f7f2a63b3d39c1eb49
MoatazBadawy <moataz.egy101@gmail.com>
2020-05-27 06:08:11 +0200

    finish image fragment layouts

--- app/src/main/java/com/picsapp/moamenapp/Fragment/ImagesFragmentProject/WallpapersImagesFragment.java
+++ app/src/main/java/com/picsapp/moamenapp/Fragment/ImagesFragmentProject/WallpapersImagesFragment.java
@@ -56,6 +56,7 @@ public class WallpapersImagesFragment extends Fragment {
                 "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg", "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
                 "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg", "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
                 "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg", "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+                "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
                 "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg", "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
         };
 
